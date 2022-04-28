21f7c005aadccf61e30f63e0d2cf1ea1e2ebd154
sarr9006 <103977447+sarr9006@users.noreply.github.com>
2022-04-27 23:21:26 -0400

    Update Beanie.java

--- src/Beanie.java
+++ src/Beanie.java
@@ -1,4 +1,15 @@
 
 public class Beanie extends Product{
     ProductCategory category = ProductCategory.Beanie;
+
+    public Beanie(){
+        this.price = 0;
+        this.numberInStock = 1;
+    }
+
+    public Beanie(double price, int stockCount, String color){
+        this.price = price;
+        this.numberInStock = stockCount;
+        this.color = color;
+    }
 }
